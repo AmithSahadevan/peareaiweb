a732a959d878ad0b5ca40b18f8e74d3bb84eec96
Amith S Dev <70939150+AmithSahadevan@users.noreply.github.com>
2025-04-03 13:44:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Configuration file for the application
 const config = {
-    streamlitUrl: 'https://9e3c-103-170-54-151.ngrok-free.app'  // This will be updated whenever ngrok URL changes
+    streamlitUrl: 'https://590e-2401-4900-6149-aef1-8841-6059-718b-b42f.ngrok-free.app'  // This will be updated whenever ngrok URL changes
 }; 
